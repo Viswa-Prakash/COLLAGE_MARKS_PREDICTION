00a1ce9bf745aab35c4101176f98f4497b169c8c
Viswa-Prakash <prakashviswa1990@gmail.com>
2024-01-02 11:05:41 +0530

    exception file added

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
+import sys
 from src.prediction.logger import logging
+from src.prediction.exception import CustomException
 
 if __name__=="__main__":
     logging.info("The excecution started")
+
+    try:
+        a= 1/0
+
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
+    
